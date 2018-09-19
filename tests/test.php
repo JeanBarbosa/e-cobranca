f80e97865eb02b9e0b1188d3da349000a3f42fd4
Jean Barbosa <programmer.jean@gmail.com>
2018-09-19 09:22:22 -0300

    == Removendo pasta xml

--- tests/test.php
+++ tests/test.php
@@ -10,7 +10,29 @@ use Caixa\BoletoCaixa;
 
 $boleto = new BoletoCaixa();
 
-$boleto->setCodigoBeneficiario('123456');
-// set fields ...
+$boleto->setCodigoBeneficiario('045864');
+$boleto->setUnidade('2301');
+$boleto->setIdProcesso('045864');
+$boleto->setCnpj('013.971.668/0001-28');
+$boleto->setNossoNumero('14000000105283192');
+$boleto->setNumeroDocumento('TEST0008');
+$boleto->setDataVencimento('2018-09-30');
+$boleto->setValor('1.0');
+$boleto->setTipoEspecie('02');
+$boleto->setFlagAceite('N');
+$boleto->setDataEmissao('2018-09-11');
+$boleto->setValorAbatimento('0');
+$boleto->setNumeroDias('30');
+$boleto->setCpf('04878901160');
+$boleto->setNome('Jean Barbosa dos Santos');
+$boleto->setLogradouro('E. Paranhos, Nº 10');
+$boleto->setCidade('Paranoa');
+$boleto->setUf('DF');
+$boleto->setCep('73255050');
+$boleto->setMensagemFichaCompesacao('Pagamento referente ao evento da Ajufe: 1º Encontro Internacional da AJUFE');
 
-var_dump($boleto->incluirBoleto());
+$boleto->setDebug(true);
+
+$response = $boleto->incluirBoleto();
+
+print_r($response); die;
